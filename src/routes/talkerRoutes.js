afbb91ba544317213271b3b21bd988d17fb7519e
Pedro Papini <ppapinia@gmail.com>
2022-10-04 15:19:03 -0300

    Resolve requisito 2

--- src/routes/talkerRoutes.js
+++ src/routes/talkerRoutes.js
@@ -7,8 +7,20 @@ const router = express.Router();
 const pathToTalkersFile = path.resolve(__dirname, '..', 'talker.json');
 
 router.get('/', async (_req, res) => {
-  const talkers = await fs.readFile(pathToTalkersFile);
-  res.status(200).json(JSON.parse(talkers));
+  const talkers = JSON.parse(await fs.readFile(pathToTalkersFile));
+  res.status(200).json(talkers);
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = JSON.parse(await fs.readFile(pathToTalkersFile));
+  const talkerIndex = talkers.findIndex((talker) => talker.id === Number(id));
+
+  if (talkerIndex >= 0) {
+    return res.status(200).json(talkers[talkerIndex]);
+  }
+
+  res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 });
 
 module.exports = router;
