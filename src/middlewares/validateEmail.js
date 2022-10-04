83a047c7e790f135f00a5102f3f6ce7de1f26f12
Pedro Papini <ppapinia@gmail.com>
2022-10-04 15:42:19 -0300

    Resolve requisito 4

---
+++ src/middlewares/validateEmail.js
@@ -0,0 +1,13 @@
+module.exports = (req, res, next) => {
+  const { email } = req.body;
+
+  if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+
+  const emailRegex = /\w+@\w+\.com/;
+
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+
+  next();
+};
--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const validateEmail = require('../middlewares/validateEmail');
+const validatePassword = require('../middlewares/validatePassword');
 const generateToken = require('../utils/generateToken');
 
 const router = express.Router();
 
-router.post('/', (_req, res) => {
+router.post('/', validateEmail, validatePassword, (_req, res) => {
   const token = generateToken();
 
   res.status(200).json({ token });
